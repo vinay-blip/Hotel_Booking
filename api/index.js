b00673b8fd13175775617e3e9e3339df64d0a91d
jitendra <jitander51@gmail.com>
2022-05-28 10:49:31 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,56 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js"
+import usersRoute from "./routes/users.js"
+import roomsRoute from "./routes/rooms.js"
+import hotelsRoute from "./routes/hotels.js"
+
+const app = express();
+dotenv.config();
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB");
+        // console.log(process.env.MONGO);
+
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on("disconnected", () => {
+    console.log("mongoDB disconnected");
+})
+
+// Middlewares
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+// Error Handling Middleware
+app.use((err,req,res,next)=>{
+
+//  Method 1   
+//    return res.status(500).json("hello error from handler");
+
+// Method 2
+const errorStatus = err.status || 500;
+const errorMessage = err.message || "Something went wrong";
+return res.status(errorStatus).json({
+    Success:false,
+    status:errorStatus,
+    message:errorMessage,
+    stack:err.stack,
+});
+});
+
+app.listen(8800, () => {
+    connect();
+    console.log("connected to backend");
+})
---
+++ api/utils/error.js
@@ -0,0 +1,7 @@
+// Error Handling
+export const createError = (status, message) => {
+    const err = new Error();
+    err.status = status;
+    err.message = message;
+    return err;
+};
