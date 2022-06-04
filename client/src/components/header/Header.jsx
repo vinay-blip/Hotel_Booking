23bdfcdfcf1b347045931359ddced30cb809e194
unknown <vaishalidhooper29@gamil.com>
2022-06-04 12:59:15 +0530

    UI_DESIGN

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,43 @@
+import { faBed, faCar, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+import "./header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="headerContainer">
+        <div className="headerList">
+            <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed}/>
+        <span>Stays</span>
+        </div>
+        <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane}/>
+        <span>Flights</span>
+        </div>
+        <div className="headerListItem">
+            <FontAwesomeIcon icon={faCar}/>
+        <span>Car Rentals</span>
+        </div>
+        <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed}/>
+        <span>Attractions</span>
+        </div>
+        <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi}/>
+        <span>Airport Taxis</span>
+        </div>
+        </div>
+        <h1 className="headerTitle">A lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">Get rewarded for your travels- unlock instant saving of 10% or more with a free HotelBooking account.</p>
+        <button className="headerBtn">Sign in /Register</button>
+        <div className="headerSearch">
+            
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
